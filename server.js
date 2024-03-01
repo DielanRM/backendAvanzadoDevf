386fc3196f62c5c7949a5972626e9dbffe82a6aa
DielanRM <dielan.r.m@gmail.com>
2024-02-29 21:01:39 -0500

    primer commit

---
+++ server.js
@@ -0,0 +1,16 @@
+/*  TODO PARA CREAR UNA APP EN EXPRESS
+
+    1.- importar express
+    2.- crear app con express
+    3.- usar app.listen para abrir puertos */
+
+//1
+import express from 'express';
+
+//2
+const api = express();
+
+//3
+api.listen(8000, ()=>{
+    console.log('api corriendo en puerto 8000');
+});
